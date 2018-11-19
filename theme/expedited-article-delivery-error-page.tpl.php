1d1cb6e543846ad1901736fbe5322f4fe47d43ae
Favenzio Calvo <fcalvo@fsu.edu>
2018-11-19 15:16:38 +0000

    Added success and error templates.

---
+++ theme/expedited-article-delivery-error-page.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for expedited article delivery error page.
+ */
+?>
+
+<h2>Error Page</h2>
+
+<p>We were unable to process your request.</p>
+
+<p>Error Code: <?php print $error_code; ?></p>
---
+++ theme/expedited-article-delivery-success-page.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for expedited article delivery success page.
+ */
+?>
+
+<h2>Order Successfully Processed</h2>
+
+  <p>Your order was successful!</p>
+  <p><strong>Order ID: </strong><?php print $order_id; ?><br />
+  <strong>Rnd ID: </strong><?php print $rnd_id; ?></p>
+  <p>Thank you for using the FSU Libraries Expedited Article Delivery service.</p>
+
