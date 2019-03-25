37db95d248d088530d3de2a249f03f0ab8d5ab58
Favenzio Calvo <fcalvo@fsu.edu>
2019-03-25 16:07:46 -0400

    Changed theme template.

--- theme/expedited-article-delivery-success-page.tpl.php
+++ theme/expedited-article-delivery-success-page.tpl.php
@@ -7,7 +7,7 @@
 
 <h2>Order Successfully Processed</h2>
 
-  <p>Your order was successful!</p>
+  <p>Your order was successful! You should receive an email within two hours with instructions to download the article you requested.</p>
   <p><strong>Order ID: </strong><?php print $order_id; ?><br />
   <strong>Rnd ID: </strong><?php print $rnd_id; ?></p>
   <?php print $success_message; ?>
